b6e7333fabbcbb7946cdf2eb5c0aa8bc01886791
unknown <hmstech11@gmail.com>
2025-02-24 13:21:59 +0500

    mew

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Wallet\BankAccountController;
 use App\Http\Controllers\Wallet\UserController;
 use App\Http\Controllers\WalletCurrencyController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::get('/optimize-app', function () {
+    Artisan::call('optimize:clear'); // Clears cache, config, route, and view caches
+    Artisan::call('cache:clear');    // Clears application cache
+    Artisan::call('config:clear');   // Clears configuration cache
+    Artisan::call('route:clear');    // Clears route cache
+    Artisan::call('view:clear');     // Clears compiled Blade views
+    Artisan::call('config:cache');   // Rebuilds configuration cache
+    Artisan::call('route:cache');    // Rebuilds route cache
+    Artisan::call('view:cache');     // Precompiles Blade templates
+    Artisan::call('optimize');       // Optimizes class loading
+
+    return "Application optimized and caches cleared successfully!";
+});
+
+
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']); // Register a user
     Route::post('/otp-verification', [AuthController::class, 'otpVerification']); // Verify OTP
