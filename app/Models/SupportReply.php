05bd684a01fa4557e755dc6c802616166a7b4c20
unknown <hmstech11@gmail.com>
2025-02-26 20:53:55 +0500

    ahsgd

---
+++ app/Models/SupportReply.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SupportReply extends Model
+{
+    use HasFactory;
+    protected $fillable = ['ticket_id', 'user_id', 'message', 'sender_type'];
+
+    public function ticket()
+    {
+        return $this->belongsTo(SupportTicket::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
