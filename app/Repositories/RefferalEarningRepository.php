dc4a5027aa1a2448ef767638da42cb031362a8f1
unknown <hmstech11@gmail.com>
2025-03-14 15:24:37 +0500

    hello wolrd gi

--- app/Repositories/RefferalEarningRepository.php
+++ app/Repositories/RefferalEarningRepository.php
@@ -22,9 +22,9 @@ class RefferalEarningRepository
         $user = User::find($id);
         $totalRefferals = User::where('invite_code', $user->user_code)->count();
         $totalRefferlBalance = UserAccount::where('user_id', $id)->first();
-        $data = ReferalEarning::where('user_id', $id)->with('user', 'referal')->get();
+        $data = ReferalEarning::where('user_id', $id)->with('referal')->get();
         return [
-            'data' => $data,
+            'earning' => $data,
             'totalRefferals' => $totalRefferals,
             'Earning' => [
                 'usd' => $totalRefferlBalance->total_referral_earnings,
