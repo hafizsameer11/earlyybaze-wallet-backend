04075bee5dc06da4a891d53a992017e85ec42960
unknown <hmstech11@gmail.com>
2025-03-15 21:53:52 +0500

    hello wolrd

--- database/migrations/2025_03_15_164910_update_user_table.php
+++ database/migrations/2025_03_15_164910_update_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['user','admin','agent'])->default('user');
+            $table->enum('role', ['user', 'admin', 'agent'])->default('user');
         });
     }
 
