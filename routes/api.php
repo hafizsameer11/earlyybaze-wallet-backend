db85d3dfec18ee6e3c154163f6a4241759a69d99
unknown <hmstech11@gmail.com>
2025-04-12 00:54:41 +0500

    hello wolrd

--- routes/api.php
+++ routes/api.php
@@ -95,8 +95,7 @@ Route::prefix('auth')->group(function () {
 
 });
 Route::post('/user/set-pin', [UserController::class, 'setPin']);
-Route::post('/user/verify-pin', [UserController::class, 'ver
-ifyPin']);
+Route::post('/user/verify-pin', [UserController::class, 'verifyPin']);
 Route::post('/webhook', [WebhookController::class, 'webhook']);
 Route::post('/admin/login', [AuthController::class, 'adminLogin']);
 
