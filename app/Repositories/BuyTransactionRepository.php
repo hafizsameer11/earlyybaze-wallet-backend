3ab59be13280f414f8a14a4e0747789b7c20b3e8
unknown <hmstech11@gmail.com>
2025-04-17 15:49:21 +0500

    hello wolrd

--- app/Repositories/BuyTransactionRepository.php
+++ app/Repositories/BuyTransactionRepository.php
@@ -143,7 +143,7 @@ class BuyTransactionRepository
                 'name' => $account->currency,
                 'symbol' => $account->walletCurrency->symbol,
                 'icon' => $account->walletCurrency->icon,
-                'network' => $account->walletCurrency->network,
+                'network' => $account->walletCurrency->blockchain,
                 'balance' => $account->available_balance,
                 'account_balance' => $account->account_balance,
                 'price' => $price,
