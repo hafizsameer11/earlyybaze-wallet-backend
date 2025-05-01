e31dd9051a4d116159c1c79e7907dc3e66ff9280
unknown <hmstech11@gmail.com>
2025-05-01 22:18:59 +0500

    helo wolrD

--- app/Repositories/UserAccountRepository.php
+++ app/Repositories/UserAccountRepository.php
@@ -23,7 +23,7 @@ class UserAccountRepository
 
         $userVirtualAccounts = VirtualAccount::where('user_id', $id)
             ->with('walletCurrency')
-            ->whereIn('currency', $currencies)
+        
             ->get();
 
         $totalCryptoUsd = '0';
