fb7b5b6aa89ea16433e0da2aad4f67648338602d
unknown <hmstech11@gmail.com>
2025-05-14 03:28:20 +0500

    helo word

---
+++ database/migrations/2025_05_13_222537_create_user_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->text('message')->nullable();
+            $table->string('status')->nullable()->default('unread');
+            $table->string('type')->nullable()->default('system');
+            $table->string('attachment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_notifications');
+    }
+};
