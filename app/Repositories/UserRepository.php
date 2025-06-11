2d391555961844eb98f7f20c75bd6bad9e7bfc29
Your Name <you@example.com>
2025-06-11 11:23:26 +0500

    helllo wolrd

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -95,7 +95,7 @@ class UserRepository
             $exchangeRate=ExchangeRate::where('currency',$account->currency)->first();
             $amountUsd=$account->walletCurrency->price;
             if($exchangeRate){
-                $amountUsd=bcmul($account->available_balance, $exchangeRate->rate_usd,4);
+                $amountUsd=bcmul($account->available_balance, $exchangeRate->rate_usd,2);
             }
             return [
                 'id' => $account->id,
