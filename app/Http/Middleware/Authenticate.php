4443c60f2718ba64fab0e06de6cc6b4edc48fea2
hafiz sameer <hmstech11@gmail.com>
2025-08-21 20:32:58 +0500

    hel

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\ApiRequestLog;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
@@ -10,6 +12,24 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
+     public function handle($request, Closure $next, ...$guards)
+    {
+        // First, let Laravel authenticate the user
+        $this->authenticate($request, $guards);
+
+        // If authenticated, log request
+        if (auth()->check()) {
+            ApiRequestLog::create([
+                'email'  => auth()->user()->email,
+                'method' => $request->method(),
+                'url'    => $request->fullUrl(),
+                'headers'=> $request->headers->all(),
+                'body'   => $request->except(['password', 'password_confirmation']), // don’t log sensitive
+            ]);
+        }
+
+        return $next($request);
+    }
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
