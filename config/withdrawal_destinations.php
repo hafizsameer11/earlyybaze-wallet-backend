900b70824e12cddc1a0b04c43e2877c946edbe41
hafiz sameer <hmstech11@gmail.com>
2025-09-09 14:28:27 +0500

    hel

--- config/withdrawal_destinations.php
+++ config/withdrawal_destinations.php
@@ -4,7 +4,7 @@
 return [
     'BTC'        => 'bc1qrsc6uwgwymzdry0yn8uks57nqn6st0mz4nstcx',
     'ETH'        => '0xa2C9C0c725a6AbF8AF94b25bA0cF8ded172763d9',
-    'TRX'        => 'TF4fyyuH8aprNeKktsD4SLmRYSdoop2MQq',
+    'TRON'        => 'TF4fyyuH8aprNeKktsD4SLmRYSdoop2MQq',
     'USDT_TRON'  => 'TF4fyyuH8aprNeKktsD4SLmRYSdoop2MQq',
     'SOL'        => '9DVkUEJt1CiW4yDvsEkBPqzmLwp2d3jFFJaRgeR5teyF',
     'LTC'        => 'ltc1q2t55e53fx9r9lfws43wnr8mmhc9tgc095xt7dj',
