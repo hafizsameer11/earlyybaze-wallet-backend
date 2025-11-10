4c8b025109a4a3d873e18e9381321f66fe91d61d
hafiz sameer <hmstech11@gmail.com>
2025-11-11 01:57:26 +0500

    bbank account soft delete implemented

--- app/Models/BankAccount.php
+++ app/Models/BankAccount.php
@@ -4,16 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // ← Import SoftDeletes
 
 class BankAccount extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes; // ← Use SoftDeletes
+
     protected $fillable = [
         'user_id',
         'account_number',
         'account_name',
-        'bank_name','is_default'
+        'bank_name',
+        'is_default',
     ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
