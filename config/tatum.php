1cf8af40d5dd8380d30d3653322c3761f742bcef
sohaib11222 <sohaibahmad3277@gmail.com>
2026-01-31 04:16:55 +0500

    webhook updated

--- config/tatum.php
+++ config/tatum.php
@@ -2,5 +2,5 @@
 return [
     'api_key' => env('TATUM_API_KEY', ''),
     'base_url' => env('TATUM_BASE_URL', 'https://api.tatum.io/v3'),
-    'webhook_url' => 'https://earlybaze.hmstech.xyz/api/webhook',
+    'webhook_url' => 'https://api.settlesys.com/api/webhook',
 ];
